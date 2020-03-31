29845b86f1706f759be4263c4b177eba6d0f6f1d
Guilherme Alves <gsag.dh@gmail.com>
2020-03-30 23:56:58 -0300

    Adding some flex classes to header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,9 +2,14 @@
   	<header>
 		<div class="header-main-info col-8 d-flex flex-column align-items-start">
 			<h1>{{headerTitle}}</h1>
-			<h2>{{headerSubTitle}}</h2>
+			<h2 class="d-flex flex-row">
+        <span v-for="subtitle in headerSubTitles" 
+              v-bind:key="subtitle">
+          {{subtitle}}
+        </span>
+      </h2>
 		</div>
-		<div class="header-side-info col-4 d-flex flex-column align-items-end">
+		<div class="header-side-info col-4 d-flex flex-column align-items-end justify-content-center">
 			<span class="header-side-info__email">
 				{{headerEmail}}
 			</span>
@@ -22,40 +27,44 @@
 import * as VMasker from "@/assets/libs/vanilla-masker.min.js";
 
 export default {
-  name: "Header",
-  data: () => {
-    return {
-      headerTitle: "Guilherme da Silva Alves Gonçalves",
-      headerSubTitle: ["Líder de Projetos", "Desenvolvedor Web"],
-      headerEmail: "gsag.dh@gmail.com",
-      headerPhone: 21996760304,
-      headerCity: "Niterói / RJ"
-    };
-  },
-  methods: {
-    formatPhone: phoneNumber => {
-      return VMasker.toPattern(phoneNumber, "(99) 9 9999-9999");
-    }
-  }
+	name: "Header",
+	data: () => {
+		return {
+			headerTitle: "Guilherme da Silva Alves Gonçalves",
+			headerSubTitles: ["Líder de Projetos", "Desenvolvedor Web"],
+			headerEmail: "gsag.dh@gmail.com",
+			headerPhone: 21996760304,
+			headerCity: "Niterói / RJ"
+		};
+	},
+	methods: {
+		formatPhone: phoneNumber => {
+			return VMasker.toPattern(phoneNumber, "(99) 9 9999-9999");
+		}
+	}
 };
 </script>
 
 <style lang="css" scoped>
 h1 {
-  font-weight: bold;
+	font-weight: bold;
 }
 h2,
 span {
-  font-weight: 300;
+	font-weight: 300;
+}
+h2 > span:not(:first-child)::before {
+	content: "|";
+	margin: 0 0.25em 0 0.5em;
 }
 header {
-  padding: 0 1em;
+	padding: 0 1em;
 }
 header::after {
-  content: "";
-  height: 2px;
-  background-color: var(--main-text-color);
-  width: 100%;
-  margin: 0.5em 1em;
+	content: "";
+	height: 2px;
+	background-color: var(--main-text-color);
+	width: 100%;
+	margin: 0.5em 1em;
 }
 </style>
